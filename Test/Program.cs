3f7f51cbfe9aabaaa68dfe9423ce10f110c33f4f
chakilo <chakilo@msn.com>
2018-02-23 15:20:48 +0800

    no message

--- Test/Program.cs
+++ Test/Program.cs
@@ -7,7 +7,7 @@ namespace Test {
         [MTAThread]
         static void Main(string[] args) {
             Console.WriteLine("Hello World!\n\n");
-            JtSQL.Run(new Work("// find users that user_id > 0 and < 5\n$var users = <SELECT * FROM `user` WHERE user_id > {{0}} AND user_id < {{Math.abs(5)}};>\nfor (;;) {if (condition) {var result = $<SELECT * FROM {{table}}>}}"));
+            JtSQL.Run(new Work("// find users that user_id > 0 and < 5\nvar users = $<SELECT * FROM `user` WHERE user_id > {{0}} AND user_id < {{Math.abs(5)}};>\nfor (;;) {if (condition) {var result = $<SELECT * FROM {{table}}>}}"));
             Console.ReadLine();
         }
     }
